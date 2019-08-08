098f30226bea447f77dad00bd4355ed60e629ade
Aaron Barnard <aaronbarnardsound@gmail.com>
2019-08-08 15:30:47 +1000

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import babel from "rollup-plugin-babel"
+import resolve from "rollup-plugin-node-resolve"
+import json from "rollup-plugin-json"
+import builtins from "rollup-plugin-node-builtins"
+import commonjs from "rollup-plugin-commonjs"
+import { terser } from "rollup-plugin-terser"
+
+let plugins = [
+  resolve({
+    preferBuiltins: true
+  }),
+  babel({
+    exclude: "node_modules/**"
+  }),
+  json(),
+  commonjs(),
+  builtins(),
+  terser()
+]
+export default {
+  input: "lib/index.js",
+  output: {
+    sourcemap: true,
+    format: "cjs",
+    name: "Blocknative",
+    file: "dist/blocknative.js"
+  },
+  plugins: plugins
+}
