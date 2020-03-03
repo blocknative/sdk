e6986b95dcf8a8073dbec6ab7dd2ab3c6bf7aec8
Aaron <abarnard@protonmail.com>
2020-03-03 15:32:00 +1100

    Make clientIndex optional

--- src/account.ts
+++ src/account.ts
@@ -6,8 +6,8 @@ import { Emitter, Ac } from './interfaces'
 import { validateAccount } from './validation'
 
 function account(
-  clientIndex: number,
-  address: string
+  address: string,
+  clientIndex: number = 0
 ): { emitter: Emitter; details: { address: string } } {
   validateAccount(clientIndex, address)
 
