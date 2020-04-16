c6cb7be243d6a242f07fc3b9a2fe34b723e39f45
Aaron <abarnard@protonmail.com>
2020-04-17 09:15:21 +1000

    Fix build outputs

--- rollup.config.js
+++ rollup.config.js
@@ -3,32 +3,37 @@ import json from '@rollup/plugin-json'
 import typescript from 'rollup-plugin-typescript2'
 import commonjs from '@rollup/plugin-commonjs'
 
-const pkg = require('./package.json')
-
 export default [
   {
     input: `src/index.ts`,
     output: {
-      file: pkg.main,
+      dir: 'dist/iife/',
+      format: 'iife',
       name: 'bncSdk',
-      format: 'umd',
-      globals: ['SturdyWebSocket']
+      globals: ['SturdyWebSocket', 'crypto-es'],
     },
     plugins: [
       json(),
       resolve(),
       commonjs(),
-      typescript({ useTsconfigDeclarationDir: true })
-    ]
+      typescript({ useTsconfigDeclarationDir: true, clean: true }),
+    ],
   },
   {
     input: `src/index.ts`,
-    output: { file: pkg.module, format: 'esm' },
+    output: [
+      {
+        format: 'esm',
+        dir: 'dist/esm/',
+      },
+      { format: 'cjs', dir: 'dist/cjs/' },
+    ],
     external: ['sturdy-websocket', 'crypto-es'],
     plugins: [
       json(),
       resolve(),
-      typescript({ useTsconfigDeclarationDir: true })
-    ]
-  }
+      commonjs(),
+      typescript({ useTsconfigDeclarationDir: true, clean: true }),
+    ],
+  },
 ]
