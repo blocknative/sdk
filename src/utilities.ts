357df2daf3802d5f2fb4898aee2a78e70119473e
Aaron Barnard <abarnard@protonmail.com>
2021-04-14 10:35:39 +1000

    3.2.0-0.1.0: [feature] - Pending Simulation Event Code (#111)
    * Add txPoolSimulation event code
    
    * Increment version

--- src/utilities.ts
+++ src/utilities.ts
@@ -22,6 +22,7 @@ export function createEmitter(): Emitter {
         case 'txSendFail':
         case 'txError':
         case 'txUnderPriced':
+        case 'txPoolSimulation':
         case 'all':
           break
         default:
