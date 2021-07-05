f1a6fd8bf1208751ec3e69f7bdda35d27a3ea59a
Aaron <abarnard@protonmail.com>
2021-07-05 11:25:36 +1000

    pass error to handler if it exists

--- src/configuration.ts
+++ src/configuration.ts
@@ -43,12 +43,19 @@ function configuration(
   return new Promise((resolve, reject) => {
     subscription.pipe(take(1), timeout(5000)).subscribe({
       next: () => resolve({ ...emitter, details: { config } }),
-      error: (error: { message: string }) =>
-        reject(
+      error: (error: { message: string }) => {
+        const message =
           error.message === 'Timeout has occurred'
             ? `Configuration with scope: ${config.scope} has been sent to the Blocknative server, but has not received a reply within 5 seconds.`
             : error.message
-        )
+
+        if (this._onerror) {
+          this._onerror({ message })
+          resolve(`Error: ${message}`)
+        } else {
+          reject(message)
+        }
+      }
     })
   })
 }
