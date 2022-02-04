96b489386f13903cef048027b5a63af7338b32ce
Adam Carpenter <adamcarpenter86@gmail.com>
2022-02-04 14:28:50 -0700

    Add gasGwei values

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -66,6 +66,10 @@ export interface EthereumTransactionData extends CommonTransactionData {
   replaceHash?: string
   counterparty?: string
   direction?: string
+  baseFeePerGasGwei?: number
+  maxPriorityFeePerGasGwei?: number
+  maxFeePerGasGwei?: number
+  gasPriceGwei?: number
 }
 
 export interface InternalTransaction {
