ef8014a0e4f7d91bcaa67fdc61f6476d5d7cc446
julianapeace <chancecordelia@gmail.com>
2022-03-02 11:01:34 -0600

    bare bones

--- src/simulate.ts
+++ src/simulate.ts
@@ -1,51 +1,17 @@
 import { Ac, Tx } from './interfaces'
 import { isAddress, isTxid } from './utilities'
 
-function simulate(this: any, addressOrHash: string) {
+function simulate(this: any, payload: any) {
   if (this._destroyed)
     throw new Error(
       'The WebSocket instance has been destroyed, re-initialize to continue making requests.'
     )
 
-  const address = isAddress(this._system, addressOrHash)
-  const txid = isTxid(this._system, addressOrHash)
-
-  // check if it is an address or a hash
-  if (address) {
-    const normalizedAddress =
-      this._system === 'ethereum' ? addressOrHash.toLowerCase() : addressOrHash
-    // remove address from accounts
-    this._watchedAccounts = this._watchedAccounts.filter(
-      (ac: Ac) => ac.address !== normalizedAddress
-    )
-
-    // logEvent to server
-    this._sendMessage({
-      categoryCode: 'simulate',
-      eventCode: 'txSimulation',
-      account: {
-        address: normalizedAddress
-      }
-    })
-  } else if (txid) {
-    // remove transaction from transactions
-    this._watchedTransactions = this._watchedTransactions.filter(
-      (tx: Tx) => tx.hash !== addressOrHash
-    )
-
-    // logEvent to server
-    this._sendMessage({
-      categoryCode: 'activeTransaction',
-      eventCode: 'unwatch',
-      transaction: {
-        [this._system === 'ethereum' ? 'hash' : 'txid']: addressOrHash,
-        id: addressOrHash,
-        status: 'unsubscribed'
-      }
-    })
+  if (payload) {
+    // send payload to server
   } else {
     throw new Error(
-      `Error trying to simulate ${addressOrHash}: not a valid address or transaction id/hash`
+      `Error trying to simulate ${payload}`
     )
   }
 }
