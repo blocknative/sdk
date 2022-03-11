c1986b6ed7626be8b7e75f23795ac08524c49c08
julianapeace <chancecordelia@gmail.com>
2022-03-11 11:04:51 -0600

    undo test changes in configuration

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,24 +1,23 @@
 import { Subject } from 'rxjs'
 import { take, timeout } from 'rxjs/operators'
-import { Config, Emitter, SimulationTransaction } from './interfaces'
+import { Config, Emitter } from './interfaces'
 import { createEmitter } from './utilities'
-
 
 function configuration(
   this: any,
-  transaction: SimulationTransaction,
-): Promise<string | { details: { }; emitter?: Emitter }> {
+  config: Config
+): Promise<string | { details: { config: Config }; emitter?: Emitter }> {
   if (this._destroyed) {
     throw new Error(
       'The WebSocket instance has been destroyed, re-initialize to continue making requests.'
     )
   }
-  //
-  // const casedScope =
-  //   this._system === 'ethereum' ? config.scope.toLowerCase() : config.scope
-  //
-  // // resolve previous configuration if exists
-  // const previousConfiguration = this._configurations.get(casedScope)
+
+  const casedScope =
+    this._system === 'ethereum' ? config.scope.toLowerCase() : config.scope
+
+  // resolve previous configuration if exists
+  const previousConfiguration = this._configurations.get(casedScope)
 
   previousConfiguration &&
     previousConfiguration.subscription &&
@@ -27,27 +26,23 @@ function configuration(
   const subscription = new Subject()
 
   // create emitter for transaction
-  const emitter = { emitter: createEmitter() }
-  //
-  // this._configurations.set(casedScope, {
-  //   ...config,
-  //   ...emitter,
-  //   subscription
-  // })
+  const emitter = config.watchAddress ? { emitter: createEmitter() } : {}
+
+  this._configurations.set(casedScope, {
+    ...config,
+    ...emitter,
+    subscription
+  })
 
   this._sendMessage({
-    // categoryCode: 'configs',
-    // eventCode: 'put',
-    // config
-
-    categoryCode: 'simulate',
-    eventCode: 'txSimulation',
-    transaction
+    categoryCode: 'configs',
+    eventCode: 'put',
+    config
   })
 
   return new Promise((resolve, reject) => {
     subscription.pipe(take(1), timeout(5000)).subscribe({
-      next: () => resolve({ ...emitter, details: { transaction } }),
+      next: () => resolve({ ...emitter, details: { config } }),
       error: (error: { message: string }) => {
         const message =
           error.message === 'Timeout has occurred'
