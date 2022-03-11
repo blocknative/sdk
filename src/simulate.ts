56da5a7be6ea5e24d39fcb839282677094e6a204
julianapeace <chancecordelia@gmail.com>
2022-03-11 11:46:04 -0600

    small fix

--- src/simulate.ts
+++ src/simulate.ts
@@ -19,26 +19,6 @@ function simulate(this: any, system: string, network: string, transaction: Simul
       `Error trying to simulate ${transaction}`
     )
   }
-
-
-  return new Promise((resolve, reject) => {
-    subscription.pipe(take(1), timeout(5000)).subscribe({
-      next: () => resolve({ ...emitter, details: { config } }),
-      error: (error: { message: string }) => {
-        const message =
-          error.message === 'Timeout has occurred'
-            ? `Configuration with scope: ${config.scope} has been sent to the Blocknative server, but has not received a reply within 5 seconds.`
-            : error.message
-
-        if (this._onerror) {
-          this._onerror({ message })
-          resolve(`Error: ${message}`)
-        } else {
-          reject(message)
-        }
-      }
-    })
-  })
 }
 
 export default simulate
