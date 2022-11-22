2e18e51a776845e5cde794fffee5ed0e9ad762af
Aaron <abarnard@protonmail.com>
2022-11-23 08:36:31 +1100

    Add error logs for deprecated networks

--- src/defaults.ts
+++ src/defaults.ts
@@ -10,6 +10,8 @@ export const networks: { [key: string]: { [key: string]: string } } = {
   }
 }
 
+export const DEPRECATED_NETWORK_IDS = [2, 42, 56, 250]
+
 export const DEFAULT_RATE_LIMIT_RULES = {
   points: 150,
   duration: 1
